13e91f66d8767e9ba080015cde369a7c6dc6c739
Biplob <ghatakbits@gmail.com>
2025-03-01 20:34:48 +0530

    Add some components

---
+++ 01basics/src/First.jsx
@@ -0,0 +1,23 @@
+
+const FirstApp = () =>{
+
+    return(
+      <h2>This is my first react code</h2>
+    )
+    
+    }
+  
+    const SecondApp = () => {
+        return(
+            <h3>This will be my Second code in React</h3>
+        )
+     
+    }
+    const ThirdApp = () => {
+        return(
+            <h4>This will be my Third code in React</h4>
+        )
+     
+    }
+
+    export  {FirstApp, SecondApp, ThirdApp} 
